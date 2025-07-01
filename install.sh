dd2c79e8e2b31d554f9e7c2666c3404f25582172
Andrês <andresroliveira98@gmail.com>
2025-06-30 22:28:38 -0300

    Comecando vida nova no hilbert

--- install.sh
+++ install.sh
@@ -1,35 +1,50 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
-# Verifing the dependencies
+# Sai imediatamente se um comando falhar
+set -e
 
-if ! command -v git &> /dev/null
-then
-    echo "git could not be found"
-    exit
-fi
+create_symlink() {
+    local target=$1
+    local link_name=$2
 
-if ! command -v stow &> /dev/null
-then
-    echo "stow could not be found"
-    exit
-fi
+    # Verifica se o link já existe
+    if [ -e "$link_name" ]; then
+        # Pergunta ao usuário se quer sobrescrever
+        read -p "O link '$link_name' já existe. Deseja sobrescrever? (s/N) " confirm_overwrite
+        
+        # Converte a resposta para minúsculas e verifica
+        if [ "$confirm_overwrite" = "s" ] || [ "$confirm_overwrite" = "S" ]; then
+            echo "Sobrescrevendo link '$link_name'..."
+            # Remove o link existente antes de criar um novo
+            rm "$link_name"
+            if ln -sn "$target" "$link_name"; then
+                echo "Link simbólico '$link_name' sobrescrito com sucesso."
+            else
+                echo "ERRO: Falha ao sobrescrever o link simbólico '$link_name'." >&2
+            fi
+        else
+            echo "Operação cancelada para '$link_name'. Ignorando."
+        fi
+    else
+        echo "Criando link simbólico de '$target' para '$link_name'..."
+        # Cria o link simbólico
+        if ln -sn "$target" "$link_name"; then
+            echo "Link simbólico '$link_name' criado com sucesso."
+        else
+            echo "ERRO: Falha ao criar o link simbólico '$link_name'." >&2
+        fi
+    fi
+}
 
-stow -S git
-stow -S vim
-stow -S tmux
-stow -S nvim
-stow -S zathura
-stow -S i3
-stow -S hypr
-stow -S waybar
-stow -S wlogout
-stow -S rofi
-stow -S alacritty
-stow -S rofi
-stow -S sway
-stow -S bat
-stow -S i3status
-stow -S dunst
-stow -S nvim
-stow -S emacs
+# --- Chamadas da função ---
+create_symlink ~/dotfiles/i3status ~/.config/i3status
+create_symlink ~/dotfiles/ghostty ~/.config/ghostty
+create_symlink ~/dotfiles/sway ~/.config/sway
+create_symlink ~/dotfiles/emacs ~/.emacs.d
+create_symlink ~/dotfiles/vimrc ~/.vimrc
+create_symlink ~/dotfiles/zathura ~/.config/zathura
+create_symlink ~/dotfiles/chrome-flags.conf ~/.config/chrome-flags.conf
+create_symlink ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+create_symlink ~/dotfiles/waybar ~/.config/waybar
+create_symlink ~/dotfiles/git/gitconfig ~/.gitconfig
 
