ad36db88728cf451170c29cad7e109dfbc574210
Andrês <andresroliveira98@gmail.com>
2025-07-14 10:20:02 -0300

    Configurações adicionais

--- emacs/custom.el
+++ emacs/custom.el
@@ -4,7 +4,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(auctex company flycheck gruvbox-theme ido-completing-read+ json-mode
+            lsp-pyright lsp-ui lua-mode magit move-text
+            multiple-cursors pdf-tools python-mode quelpa
+            rainbow-delimiters smex tree-sitter-langs typescript-mode
+            yasnippet-snippets)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
