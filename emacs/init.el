4574f83429e58bf5b7963e50416d6b7554c15e7f
Andrês <andresroliveira98@gmail.com>
2025-07-15 18:20:27 -0300

    [emacs] duplicate line

--- emacs/init.el
+++ emacs/init.el
@@ -73,6 +73,19 @@
   (dired-listing-switches "-alh --group-directories-first")
   (dired-mouse-drag-files t))
 
+(defun my/duplicate-line ()
+nn  "Duplicate current line"
+  (interactive)
+  (let ((column (- (point) (point-at-bol)))
+        (line (let ((s (thing-at-point 'line t)))
+                (if s (string-remove-suffix "\n" s) ""))))
+    (move-end-of-line 1)
+    (newline)
+    (insert line)
+    (move-beginning-of-line 1)
+    (forward-char column)))
+
+(global-set-key (kbd "C-,") 'my/duplicate-line)
 
 ;; Sintax
 (turn-on-font-lock)
