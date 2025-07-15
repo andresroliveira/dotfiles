ebd8a9b5c209a6d2ca79e95b88c4539536d47f68
Andrês <andresroliveira98@gmail.com>
2025-07-15 19:52:39 -0300

    [emacs] Jupyter

--- emacs/custom.el
+++ emacs/custom.el
@@ -6,10 +6,10 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    '(auctex company flycheck gruvbox-theme ido-completing-read+ json-mode
-            lsp-pyright lsp-ui lua-mode magit move-text
+            jupyter lsp-pyright lsp-ui lua-mode magit move-text
             multiple-cursors pdf-tools python-mode quelpa
             rainbow-delimiters smex tree-sitter-langs typescript-mode
-            yasnippet-snippets)))
+            yapfify yasnippet-snippets)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
